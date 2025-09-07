321d5b39e1251c0d7c1776c3eb2dd872aabc3e42
roshansaini18 <135322753+roshansaini18@users.noreply.github.com>
2025-09-08 03:44:11 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
-  base: '/', // 👈 ensures assets are loaded correctly no matter the route
-  server: {
-    port: 5173,
-    open: true,
-    historyApiFallback: true,
-  },
+  plugins: [react(),
+    tailwindcss()
+  ],
 })
